a175948ad2d4ee7962d3544c7f931981490db9b3
Christian Wolf <christian.wolf1@arvato.com>
2017-11-05 21:25:18 +0100

    add missing tests, beautify example, add del function

--- example.js
+++ example.js
@@ -3,32 +3,26 @@ const datastore = require("./index.js")({
   port: 6379
 });
 
-datastore
-  .set("some key", "some value")
-  .then(result => {
-    console.log(result);
-    return datastore.get("some key");
-  })
-  .then(result => {
-    console.log(result);
-    return datastore.addToSet("some event", "some value");
-  })
-  .then(result => {
-    console.log(result);
-    return datastore.addToSet("some event", "some other value");
-  })
-  .then(result => {
-    console.log(result);
-    return datastore.getSet("some event");
-  })
-  .then(result => {
-    console.log(result);
-    return datastore.set("some number", 1);
-  })
-  .then(result => {
-    console.log(result);
-    return datastore.incr("some number");
-  })
-  .then(result => {
-    console.log(result);
-  });
+const run = async () => {
+  const set = await datastore.set("some key", 1);
+  console.log(`set returned with ${set}`);
+  const get = await datastore.get("some key");
+  console.log(`get returned with ${get}`);
+  const addToSet = await datastore.addToSet("some event", "some value");
+  console.log(`addToSet returned with ${addToSet}`);
+  const secondAddToSet = await datastore.addToSet(
+    "some event",
+    "some other value"
+  );
+  console.log(`second addToSet returned with ${secondAddToSet}`);
+  const getSet = await datastore.getSet("some event");
+  console.log(`getSet returned with ${getSet}`);
+  const incr = await datastore.incr("some key");
+  console.log(`incr returned with ${incr}`);
+  const del = await datastore.del("some key");
+  console.log(`del returned with ${del}`);
+  const getAfterDelete = await datastore.get("some key");
+  console.log(`get after delete returned with ${getAfterDelete}`);
+};
+
+run();
--- test/datastore.spec.js
+++ test/datastore.spec.js
@@ -1,25 +1,71 @@
 const Lab = require("lab");
 const lab = (exports.lab = Lab.script());
-const { before, it, describe } = lab;
+const { beforeEach, it, describe } = lab;
 const { expect } = require("code");
 const pquire = require("proxyquire").noPreserveCache();
 const sinon = require("sinon");
 
 describe("datastore", () => {
+  let clientStub;
+  let redisStub;
+  let datastoreStub;
+
+  beforeEach(() => {
+    clientStub = {
+      setAsync: sinon.stub().resolves(),
+      getAsync: sinon.stub().resolves(),
+      saddAsync: sinon.stub().resolves(),
+      smembersAsync: sinon.stub().resolves(),
+      incrAsync: sinon.stub().resolves(),
+      delAsync: sinon.stub().resolves()
+    };
+
+    redisStub = {
+      redis: {
+        createClient: sinon.stub().returns(clientStub)
+      }
+    };
+
+    datastoreStub = pquire("../lib/datastore.js", redisStub);
+  });
+
   it("should initialize redis client with options", () => {
     const options = {
       host: "127.0.0.1",
       port: 6379
     };
 
-    const redisStub = {
-      redis: {
-        createClient: sinon.stub()
-      }
-    };
-
-    const datastoreStub = pquire("../lib/datastore.js", redisStub);
     datastoreStub(options);
     expect(redisStub.redis.createClient.calledOnce).to.be.true();
+  });
+
+  it("should call redis setAsync on set method", async () => {
+    const result = await datastoreStub().set("key", "value");
+    expect(clientStub.setAsync.calledWith("key", "value")).to.be.true();
+  });
+
+  it("should call redis getAsync on get method", async () => {
+    const result = await datastoreStub().get("key");
+    expect(clientStub.getAsync.calledWith("key")).to.be.true();
+  });
+
+  it("should call redis saddAsync on addToSet method", async () => {
+    const result = await datastoreStub().addToSet("key", "value");
+    expect(clientStub.saddAsync.calledWith("key", "value")).to.be.true();
+  });
+
+  it("should call redis smembersAsync on getSet method", async () => {
+    const result = await datastoreStub().getSet("key");
+    expect(clientStub.smembersAsync.calledWith("key")).to.be.true();
+  });
+
+  it("should call redis incrAsync on incr method", async () => {
+    const result = await datastoreStub().incr("key");
+    expect(clientStub.incrAsync.calledWith("key")).to.be.true();
+  });
+
+  it("should call redis delAsync on del method", async () => {
+    const result = await datastoreStub().del("key");
+    expect(clientStub.delAsync.calledWith("key")).to.be.true();
   });
 });
